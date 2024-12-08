2f11fdd5774358c832db797957ca76ce8e505244
Agoston Diamont <dagoston93@gmail.com>
2024-12-08 20:12:39 +0100

    ReactQuery added to the project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SnackbarProvider } from "notistack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 30_000,
+        },
+    },
+});
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <SnackbarProvider
-            maxSnack={4}
-            autoHideDuration={5000}
-            preventDuplicate={true}
-            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        >
-            <RouterProvider router={router} />
-        </SnackbarProvider>
+        <QueryClientProvider client={queryClient}>
+            <SnackbarProvider
+                maxSnack={4}
+                autoHideDuration={5000}
+                preventDuplicate={true}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+                <RouterProvider router={router} />
+            </SnackbarProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
